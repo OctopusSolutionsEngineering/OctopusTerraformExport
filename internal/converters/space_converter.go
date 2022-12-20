ad534520ee163c53bff71edbe08d5e37473c62de
Matthew Casperson <matthewcasperson@gmail.com>
2022-12-20 10:44:24 +1000

    created provider file

--- internal/converters/space_converter.go
+++ internal/converters/space_converter.go
@@ -14,28 +14,20 @@ type SpaceConverter struct {
 }
 
 func (c SpaceConverter) ToHcl() (map[string]string, error) {
-	space := model.Space{}
-	err := c.Client.GetSpace(&space)
+
+	spaceTf, err := c.createSpaceTf()
 
 	if err != nil {
 		return nil, err
 	}
 
-	terraformResource := model.TerraformSpace{
-		Description:              space.Description,
-		IsDefault:                space.IsDefault,
-		IsTaskQueueStopped:       space.TaskQueueStopped,
-		Name:                     "octopus_space_" + util.SanitizeName(space.Name),
-		SpaceManagersTeamMembers: space.SpaceManagersTeamMembers,
-		SpaceManagersTeams:       space.SpaceManagersTeams,
-		ResourceName:             space.Name,
-		Type:                     "octopusdeploy_space",
-	}
-	file := hclwrite.NewEmptyFile()
-	file.Body().AppendBlock(gohcl.EncodeAsBlock(terraformResource, "resource"))
+	provider := c.createSpaceProvider()
+	terraformConfig := c.createTerraformConfig()
 
 	results := map[string]string{
-		internal.CreateSpaceDir + "/space.tf": string(file.Bytes()),
+		internal.CreateSpaceDir + "/space.tf":    spaceTf,
+		internal.CreateSpaceDir + "/provider.tf": provider,
+		internal.CreateSpaceDir + "/config.tf":   terraformConfig,
 	}
 
 	// Convert the projects
@@ -60,4 +52,45 @@ func (c SpaceConverter) processProjects() (map[string]string, error) {
 	return ProjectConverter{
 		Client: c.Client,
 	}.ToHcl()
+}
+
+func (c SpaceConverter) createSpaceTf() (string, error) {
+	space := model.Space{}
+	err := c.Client.GetSpace(&space)
+
+	if err != nil {
+		return "", err
+	}
+
+	terraformResource := model.TerraformSpace{
+		Description:              space.Description,
+		IsDefault:                space.IsDefault,
+		IsTaskQueueStopped:       space.TaskQueueStopped,
+		Name:                     "octopus_space_" + util.SanitizeName(space.Name),
+		SpaceManagersTeamMembers: space.SpaceManagersTeamMembers,
+		SpaceManagersTeams:       space.SpaceManagersTeams,
+		ResourceName:             space.Name,
+		Type:                     "octopusdeploy_space",
+	}
+	file := hclwrite.NewEmptyFile()
+	file.Body().AppendBlock(gohcl.EncodeAsBlock(terraformResource, "resource"))
+	return string(file.Bytes()), nil
+}
+
+func (c SpaceConverter) createSpaceProvider() string {
+	terraformResource := model.TerraformProvider{
+		Type:    "octopusdeploy",
+		Address: "var.octopusUrl",
+		ApiKey:  "var.octopusApiKey",
+	}
+	file := hclwrite.NewEmptyFile()
+	file.Body().AppendBlock(gohcl.EncodeAsBlock(terraformResource, "provider"))
+	return string(file.Bytes())
+}
+
+func (c SpaceConverter) createTerraformConfig() string {
+	terraformResource := model.TerraformConfig{}.CreateTerraformConfig()
+	file := hclwrite.NewEmptyFile()
+	file.Body().AppendBlock(gohcl.EncodeAsBlock(terraformResource, "terraform"))
+	return string(file.Bytes())
 }
---
+++ internal/model/terraform_provider.go
@@ -0,0 +1,31 @@
+package model
+
+type TerraformConfig struct {
+	RequiredProviders RequiredProviders `hcl:"required_providers,block"`
+}
+
+type RequiredProviders struct {
+	OctopusProvider OctopusProvider `hcl:"octopusdeploy"`
+}
+
+type OctopusProvider struct {
+	Source  string `cty:"source"`
+	Version string `cty:"version"`
+}
+
+type TerraformProvider struct {
+	Type    string `hcl:"type,label"`
+	Address string `hcl:"address"`
+	ApiKey  string `hcl:"api_key"`
+}
+
+func (c TerraformConfig) CreateTerraformConfig() TerraformConfig {
+	return TerraformConfig{
+		RequiredProviders: RequiredProviders{
+			OctopusProvider: OctopusProvider{
+				Source:  "OctopusDeployLabs/octopusdeploy",
+				Version: "0.9.1",
+			},
+		},
+	}
+}
