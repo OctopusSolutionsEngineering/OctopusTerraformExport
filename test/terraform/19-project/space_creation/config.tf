d8413b4b2e390fc8f8851cf63acba22535f9830e
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-07 09:46:18 +1000

    Updated provider

--- test/terraform/19-project/space_creation/config.tf
+++ test/terraform/19-project/space_creation/config.tf
@@ -1,5 +1,5 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.10.1" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.10.5" }
   }
 }
