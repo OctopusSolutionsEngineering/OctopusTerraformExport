c5c2326282cd2ec1c6eed3d01776f92149b88094
Octopus Server <octopus@octopus.com>
2023-05-16 14:12:41 +1000

    Added a test of k8s target with cert

--- cmd/internal/model/terraform/terraform_provider.go
+++ cmd/internal/model/terraform/terraform_provider.go
@@ -32,7 +32,7 @@ func (c TerraformConfig) CreateTerraformConfig(backend string, version string) T
 		RequiredProviders: RequiredProviders{
 			OctopusProvider: OctopusProvider{
 				Source:  "OctopusDeployLabs/octopusdeploy",
-				Version: strutil.DefaultIfEmpty(version, "0.12.0"),
+				Version: strutil.DefaultIfEmpty(version, "0.12.1"),
 			},
 		},
 	}
--- test/terraform/35-azurecloudservicetarget/space_creation/config.tf
+++ test/terraform/35-azurecloudservicetarget/space_creation/config.tf
@@ -1,5 +1,5 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.0" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.1" }
   }
 }
