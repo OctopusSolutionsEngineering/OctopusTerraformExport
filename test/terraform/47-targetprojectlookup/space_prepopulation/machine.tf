16674894013825a7bad67b7ab4a98f3a8aed0dc0
Matthew Casperson <matthewcasperson@gmail.com>
2023-05-31 09:50:08 +1000

    Fixed test

--- test/terraform/47-targetprojectlookup/space_prepopulation/machine.tf
+++ test/terraform/47-targetprojectlookup/space_prepopulation/machine.tf
@@ -223,19 +223,3 @@ resource "octopusdeploy_azure_web_app_deployment_target" "target_web_app" {
   thumbprint                        = ""
   web_app_slot_name                 = "slot1"
 }
-
-resource "octopusdeploy_cloud_region_deployment_target" "target_region1" {
-  environments                      = ["${octopusdeploy_environment.development_environment.id}"]
-  name                              = "Cloud"
-  roles                             = ["cloud"]
-  default_worker_pool_id            = ""
-  health_status                     = "Healthy"
-  is_disabled                       = false
-  machine_policy_id                 = "${data.octopusdeploy_machine_policies.default_machine_policy.machine_policies[0].id}"
-  shell_name                        = "Unknown"
-  shell_version                     = "Unknown"
-  tenant_tags                       = []
-  tenanted_deployment_participation = "Untenanted"
-  tenants                           = []
-  thumbprint                        = ""
-}
