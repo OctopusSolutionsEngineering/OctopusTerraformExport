9b144052fce0c1480b1cd7902b3d0c4367b77ec3
Matthew Casperson <matthewcasperson@gmail.com>
2023-09-01 12:28:02 +1000

    Updated the default TF provider version

--- cmd/internal/model/terraform/terraform_provider.go
+++ cmd/internal/model/terraform/terraform_provider.go
@@ -32,7 +32,7 @@ func (c TerraformConfig) CreateTerraformConfig(backend string, version string) T
 		RequiredProviders: RequiredProviders{
 			OctopusProvider: OctopusProvider{
 				Source:  "OctopusDeployLabs/octopusdeploy",
-				Version: strutil.DefaultIfEmpty(version, "0.12.5"),
+				Version: strutil.DefaultIfEmpty(version, "0.12.6"),
 			},
 		},
 	}
