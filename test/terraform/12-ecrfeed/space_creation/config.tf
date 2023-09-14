f24ddbf4c4b529ffe8fcbd88db38e6adff673862
Matthew Casperson <matthewcasperson@gmail.com>
2023-09-14 14:05:59 +1000

    Updated TF provider version

--- test/terraform/12-ecrfeed/space_creation/config.tf
+++ test/terraform/12-ecrfeed/space_creation/config.tf
@@ -1,5 +1,5 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.4" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.7" }
   }
 }
--- test/terraform/63-k8stargetpodauth/space_population/config.tf
+++ test/terraform/63-k8stargetpodauth/space_population/config.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.4" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.7" }
     // Use the option below when debugging
     // octopusdeploy = { source = "octopus.com/com/octopusdeploy" }
   }
