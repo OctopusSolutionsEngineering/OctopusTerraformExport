4f4bb3d129db767577552baf57d03a9813d9e742
Matthew Casperson <matthewcasperson@gmail.com>
2023-10-25 06:10:22 +1000

    Update to the latest TF provider version

--- test/terraform/30-sshtarget/space_population/config.tf
+++ test/terraform/30-sshtarget/space_population/config.tf
@@ -1,5 +1,5 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.7" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.13.0" }
   }
 }
--- test/terraform/45-k8scertauth/space_population/config.tf
+++ test/terraform/45-k8scertauth/space_population/config.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.7" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.13.0" }
     // Use the option below when debugging
     // octopusdeploy = { source = "octopus.com/com/octopusdeploy" }
   }
