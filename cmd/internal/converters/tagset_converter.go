31fd674878b5bfa4f5785fdc2e7812f15c54a0f1
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-01 10:12:51 +1000

    Add stateless tag sets

--- cmd/internal/converters/tagset_converter.go
+++ cmd/internal/converters/tagset_converter.go
@@ -15,6 +15,7 @@ import (
 
 const octopusdeployTagSetResourceType = "octopusdeploy_tag_set"
 const octopusdeployTagResourceType = "octopusdeploy_tag"
+const octopusdeployTagSetsData = "octopusdeploy_tag_sets"
 
 type TagSetConverter struct {
 	Client               client.OctopusClient
@@ -33,7 +34,7 @@ func (c TagSetConverter) ToHcl(dependencies *ResourceDetailsCollection) error {
 
 	for _, resource := range collection.Items {
 		zap.L().Info("Tagset: " + resource.Id)
-		err = c.ToHclByResource(resource, dependencies)
+		err = c.toHcl(resource, false, dependencies)
 
 		if err != nil {
 			return err
@@ -44,7 +45,14 @@ func (c TagSetConverter) ToHcl(dependencies *ResourceDetailsCollection) error {
 }
 
 func (c TagSetConverter) ToHclByResource(tagSet octopus2.TagSet, dependencies *ResourceDetailsCollection) error {
+	return c.toHcl(tagSet, false, dependencies)
+}
 
+func (c TagSetConverter) GetResourceType() string {
+	return "TagSets"
+}
+
+func (c *TagSetConverter) toHcl(tagSet octopus2.TagSet, stateless bool, dependencies *ResourceDetailsCollection) error {
 	if c.Excluder.IsResourceExcluded(tagSet.Name, false, c.ExcludeTenantTagSets, nil) {
 		return nil
 	}
@@ -56,15 +64,34 @@ func (c TagSetConverter) ToHclByResource(tagSet octopus2.TagSet, dependencies *R
 	thisResource.Id = tagSet.Id
 	thisResource.ResourceType = c.GetResourceType()
 	thisResource.Lookup = "${" + octopusdeployTagSetResourceType + "." + tagSetName + ".id}"
+
+	if stateless {
+		thisResource.Lookup = "${length(data." + octopusdeployTagSetsData + "." + tagSetName + ".tag_sets) != 0 " +
+			"? data." + octopusdeployTagSetsData + "." + tagSetName + ".tag_sets[0].id " +
+			": " + octopusdeployTagSetResourceType + "." + tagSetName + "[0].id}"
+	} else {
+		thisResource.Lookup = "${" + octopusdeployTagSetResourceType + "." + tagSetName + ".id}"
+	}
+
+	var count *string = nil
+	if stateless {
+		count = strutil.StrPointer("${length(data." + octopusdeployTagSetsData + "." + tagSetName + ".tag_sets) != 0 ? 0 : 1}")
+	}
+
 	thisResource.ToHcl = func() (string, error) {
 		terraformResource := terraform.TerraformTagSet{
 			Type:         octopusdeployTagSetResourceType,
 			Name:         tagSetName,
+			Count:        count,
 			ResourceName: tagSet.Name,
 			Description:  strutil.NilIfEmptyPointer(tagSet.Description),
 			SortOrder:    tagSet.SortOrder,
 		}
 		file := hclwrite.NewEmptyFile()
+
+		if stateless {
+			c.writeData(file, tagSet, tagSetName)
+		}
 
 		// Add a comment with the import command
 		baseUrl, _ := c.Client.GetSpaceBaseUrl()
@@ -92,10 +119,22 @@ func (c TagSetConverter) ToHclByResource(tagSet octopus2.TagSet, dependencies *R
 		tagResource.Id = tag.Id
 		tagResource.ResourceType = "Tags"
 		tagResource.Lookup = "${" + octopusdeployTagResourceType + "." + tagName + ".id}"
+
+		if stateless {
+			// There is no tag lookup, so if the tagset exists, the tag is not created, and the lookup is an
+			// empty string.
+			thisResource.Lookup = "${length(data." + octopusdeployTagSetsData + "." + tagSetName + ".tag_sets) != 0 " +
+				"? '' " +
+				": " + octopusdeployTagResourceType + "." + tagName + "[0].id}"
+		} else {
+			thisResource.Lookup = "${" + octopusdeployTagResourceType + "." + tagName + ".id}"
+		}
+
 		tagResource.ToHcl = func() (string, error) {
 			terraformResource := terraform.TerraformTag{
 				Type:         octopusdeployTagResourceType,
 				Name:         tagName,
+				Count:        count,
 				ResourceName: tag.Name,
 				TagSetId:     "${" + octopusdeployTagSetResourceType + "." + tagSetName + ".id}",
 				Color:        tag.Color,
@@ -113,6 +152,20 @@ func (c TagSetConverter) ToHclByResource(tagSet octopus2.TagSet, dependencies *R
 	return nil
 }
 
-func (c TagSetConverter) GetResourceType() string {
-	return "TagSets"
+func (c *TagSetConverter) buildData(resourceName string, resource octopus2.TagSet) terraform.TerraformTagSetData {
+	return terraform.TerraformTagSetData{
+		Type:        octopusdeployTagSetsData,
+		Name:        resourceName,
+		Ids:         nil,
+		PartialName: resource.Name,
+		Skip:        0,
+		Take:        1,
+	}
+}
+
+// writeData appends the data block for stateless modules
+func (c *TagSetConverter) writeData(file *hclwrite.File, resource octopus2.TagSet, resourceName string) {
+	terraformResource := c.buildData(resourceName, resource)
+	block := gohcl.EncodeAsBlock(terraformResource, "data")
+	file.Body().AppendBlock(block)
 }
