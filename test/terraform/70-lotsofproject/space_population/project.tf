4e9f20289d2d6c95b6b21e16efde8995866b4097
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-09 09:06:41 +1000

    Added a test to verify that the batched client works correctly

---
+++ test/terraform/70-lotsofproject/space_population/project.tf
@@ -0,0 +1,82 @@
+data "octopusdeploy_lifecycles" "lifecycle_default_lifecycle" {
+  ids          = null
+  partial_name = "Default Lifecycle"
+  skip         = 0
+  take         = 1
+}
+
+resource "octopusdeploy_project" "deploy_frontend_project" {
+  count = 100
+  auto_create_release                  = false
+  default_guided_failure_mode          = "EnvironmentDefault"
+  default_to_skip_if_already_installed = false
+  description                          = "Test project ${count.index + 1}"
+  discrete_channel_release             = false
+  is_disabled                          = false
+  is_discrete_channel_release          = false
+  is_version_controlled                = false
+  lifecycle_id                         = data.octopusdeploy_lifecycles.lifecycle_default_lifecycle.lifecycles[0].id
+  name                                 = "Test ${count.index + 1}"
+  project_group_id                     = octopusdeploy_project_group.project_group_test.id
+  tenanted_deployment_participation    = "Untenanted"
+  space_id                             = var.octopus_space_id
+  included_library_variable_sets       = []
+  versioning_strategy {
+    template = "#{Octopus.Version.LastMajor}.#{Octopus.Version.LastMinor}.#{Octopus.Version.LastPatch}.#{Octopus.Version.NextRevision}"
+  }
+
+  connectivity_policy {
+    allow_deployments_to_no_targets = false
+    exclude_unhealthy_targets       = false
+    skip_machine_behavior           = "SkipUnavailableMachines"
+  }
+}
+
+resource "octopusdeploy_deployment_process" "test" {
+  project_id = octopusdeploy_project.deploy_frontend_project[count.index].id
+  count = 100
+
+  step {
+    condition           = "Success"
+    name                = "Get MySQL Host"
+    package_requirement = "LetOctopusDecide"
+    start_trigger       = "StartAfterPrevious"
+
+    action {
+      action_type                        = "Octopus.KubernetesRunScript"
+      name                               = "Get MySQL Host"
+      condition                          = "Success"
+      run_on_server                      = true
+      is_disabled                        = false
+      can_be_used_for_project_versioning = true
+      is_required                        = false
+      worker_pool_id                     = ""
+      properties                         = {
+        "Octopus.Action.Script.ScriptBody" = "echo \"hi\""
+        "Octopus.Action.KubernetesContainers.Namespace" = ""
+        "OctopusUseBundledTooling" = "False"
+        "Octopus.Action.Script.ScriptSource" = "Inline"
+        "Octopus.Action.Script.Syntax" = "Bash"
+      }
+
+      environments          = []
+      excluded_environments = []
+      channels              = []
+      tenant_tags           = []
+
+      package {
+        name                      = "package1"
+        package_id                = "package1"
+        acquisition_location      = "Server"
+        extract_during_deployment = false
+        feed_id                   = octopusdeploy_docker_container_registry.feed_docker.id
+        properties                = { Extract = "True", Purpose = "", SelectionMode = "immediate" }
+      }
+
+      features = []
+    }
+
+    properties   = {}
+    target_roles = ["eks"]
+  }
+}
