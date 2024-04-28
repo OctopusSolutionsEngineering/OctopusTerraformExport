5c42db44bda497b0bbfb73bf599692f8b02c1dcb
Matthew Casperson <matthewcasperson@gmail.com>
2024-04-29 09:46:28 +1000

    Add support for feed triggers (#22)
    * Started work on the feed trigger
    
    * The channel is required, so added that to the test

---
+++ test/terraform/73-projectfeedtrigger/space_population/project_triggers.tf
@@ -0,0 +1,10 @@
+resource "octopusdeploy_external_feed_create_release_trigger" "my_trigger" {
+  name        = "My feed trigger"
+  space_id = var.octopus_space_id
+  project_id  = octopusdeploy_project.deploy_frontend_project.id
+  channel_id = octopusdeploy_channel.backend_mainline.id
+  package {
+    deployment_action = "Get MySQL Host"
+    package_reference = "package1"
+  }
+}
