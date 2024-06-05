cfead754d0e0ca82208bb0608abed3e45db96f02
Matthew Casperson <matthewcasperson@gmail.com>
2024-06-05 10:11:26 +1000

    Updated terraform version

--- cmd/internal/model/terraform/terraform_provider.go
+++ cmd/internal/model/terraform/terraform_provider.go
@@ -33,7 +33,7 @@ func (c TerraformConfig) CreateTerraformConfig(backend string, version string) T
 		RequiredProviders: RequiredProviders{
 			OctopusProvider: OctopusProvider{
 				Source:  "OctopusDeployLabs/octopusdeploy",
-				Version: strutil.DefaultIfEmpty(version, "0.20.0"),
+				Version: strutil.DefaultIfEmpty(version, "0.20.1"),
 			},
 		},
 		RequiredVersion: strutil.StrPointer(">= 1.6.0"),
