ea6e02c149011d867f34d2a34ddfed01f07b7c5a
Matthew Casperson <matthewcasperson@gmail.com>
2024-07-15 09:46:23 +1000

    Use json escaping to ensure secrets are injected into the HCL corerctly

--- cmd/internal/converters/variable_set_converter.go
+++ cmd/internal/converters/variable_set_converter.go
@@ -1046,9 +1046,10 @@ func (c *VariableSetConverter) writeTerraformVariablesForSecret(file *hclwrite.F
 		block := gohcl.EncodeAsBlock(secretVariableResource, "variable")
 		hcl.WriteUnquotedAttribute(block, "type", "string")
 
-		// If we are writing an octostache template, we need to have octostache replace any slashes, and not have HCL escape this template
+		// If we are writing an octostache template, we need to have octostache escape the value so it is suitable
+		// for inclusion in a string (we use JSON escaping for this)
 		if c.DefaultSecretVariableValues {
-			hcl.WriteUnquotedAttribute(block, "default", "\"#{"+variable.Name+" | Replace \"\\\\\" \"\\\\\"}\"")
+			hcl.WriteUnquotedAttribute(block, "default", "\"#{"+variable.Name+" | JsonEscape}\"")
 		}
 
 		file.Body().AppendBlock(block)
