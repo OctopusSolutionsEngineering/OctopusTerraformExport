5c6ce1b9df1c71375061f89dc552a6bcaf758cba
Matthew Casperson <matthewcasperson@gmail.com>
2024-07-15 16:36:36 +1000

    Added a test

--- cmd/internal/sanitizer/name_sanitizer_test.go
+++ cmd/internal/sanitizer/name_sanitizer_test.go
@@ -1,6 +1,9 @@
 package sanitizer
 
-import "testing"
+import (
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/data"
+	"testing"
+)
 
 func TestSanitizer(t *testing.T) {
 	sanitizedName := SanitizeName("A string with weird chars !@#$%^&*()+")
@@ -26,5 +29,13 @@ func TestSanitizerWithLeadingNumber(t *testing.T) {
 	sanitizedName := SanitizeName("0 my project")
 	if sanitizedName != "_0_my_project" {
 		t.Fatal("Sanitized name should be _0_my_project")
+	}
+}
+
+func TestSanitizeParameterName(t *testing.T) {
+	dependencies := &data.ResourceDetailsCollection{}
+	sanitizedName := SanitizeParameterName(dependencies, "0 Docker Hub", "string")
+	if sanitizedName != "_0DockerHub" {
+		t.Fatal("Sanitized name should be _0dockerhub, was " + sanitizedName)
 	}
 }
