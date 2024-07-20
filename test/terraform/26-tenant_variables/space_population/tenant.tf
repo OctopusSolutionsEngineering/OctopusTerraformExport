ed8aecdcbda7c784468c113a36c9565a303cc5bf
Matthew Casperson <matthewcasperson@gmail.com>
2024-07-20 19:54:20 +1000

    Fixed attribute

--- test/terraform/26-tenant_variables/space_population/tenant.tf
+++ test/terraform/26-tenant_variables/space_population/tenant.tf
@@ -6,7 +6,7 @@ resource "octopusdeploy_tenant" "tenant_team_a" {
 }
 
 resource "octopusdeploy_tenant_project" "tenant_project" {
-  environments = [octopusdeploy_environment.test_environment.id, octopusdeploy_environment.development_environment.id, octopusdeploy_environment.production_environment.id]
+  environment_ids = [octopusdeploy_environment.test_environment.id, octopusdeploy_environment.development_environment.id, octopusdeploy_environment.production_environment.id]
   project_id   = octopusdeploy_project.deploy_frontend_project.id
   tenant_id    = octopusdeploy_tenant.tenant_team_a.id
 }
