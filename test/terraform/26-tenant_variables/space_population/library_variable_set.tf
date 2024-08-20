f7ea6d6abcec6ad7c646051c425b38fe83ce00ed
Matthew Casperson <matthewcasperson@gmail.com>
2024-08-20 15:06:26 +1000

    Added more examples of templates

--- test/terraform/26-tenant_variables/space_population/library_variable_set.tf
+++ test/terraform/26-tenant_variables/space_population/library_variable_set.tf
@@ -8,4 +8,20 @@ resource "octopusdeploy_library_variable_set" "library_variable_set_octopus_vari
     default_value    = "a"
     display_settings = { "Octopus.ControlType" = "SingleLineText" }
   }
+
+  template {
+    name             = "Drop Down"
+    label            = "Label"
+    help_text        = "Help Text"
+    default_value    = "Value1"
+    display_settings = { "Octopus.ControlType" = "Select", "Octopus.SelectOptions" = "Value1|Display text 1\nValue2|Display text 2" }
+  }
+
+  template {
+    name             = "Check Box"
+    label            = "Check Box"
+    help_text        = "Help Text"
+    default_value    = "Default"
+    display_settings = { "Octopus.ControlType" = "Checkbox" }
+  }
 }
