dc347d0a7dbe8d5894b8d34c9819285037ef3259
Matthew Casperson <matthewcasperson@gmail.com>
2024-10-08 14:44:28 +1000

    Fixed nil edge case

--- cmd/internal/types/arrays.go
+++ cmd/internal/types/arrays.go
@@ -3,6 +3,10 @@ package types
 import "reflect"
 
 func IsArrayOrSlice(v interface{}) bool {
+	if v == nil {
+		return false
+	}
+
 	kind := reflect.TypeOf(v).Kind()
 	return kind == reflect.Array || kind == reflect.Slice
 }
