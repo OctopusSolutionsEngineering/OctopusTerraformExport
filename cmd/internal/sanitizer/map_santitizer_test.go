e9a1ad7e17027afbe18015dbc7c2b446b4d6cef3
Matthew Casperson <matthewcasperson@gmail.com>
2025-03-21 11:54:58 +1000

    Fixed test

--- cmd/internal/sanitizer/map_santitizer_test.go
+++ cmd/internal/sanitizer/map_santitizer_test.go
@@ -6,8 +6,14 @@ import (
 )
 
 func TestMapSanitizer(t *testing.T) {
-	sanitizedMap, _ := MapSanitizer{}.SanitizeMap("parent",
-		"actionname",
+	sanitizedMap, _ := MapSanitizer{}.SanitizeMap(octopus.NameId{
+		Id:      "parent",
+		SpaceId: "",
+		Name:    "parentName",
+	},
+		octopus.Action{
+			Id: "actionname",
+		},
 		map[string]any{
 			"input":  "test",
 			"input2": octopus.Variable{},
@@ -17,7 +23,7 @@ func TestMapSanitizer(t *testing.T) {
 		t.Fatal("String should be passed through with o changes")
 	}
 
-	if sanitizedMap["input2"] != "${var.parent_actionname_input2}" {
+	if sanitizedMap["input2"] != "${var.action_4481fe3a58f14368f78761e4acd1bbcaabcb3e35e596829aec0a37c086d52df8_sensitive_value}" {
 		t.Fatal("Object should be replaced with placeholder")
 	}
 }
