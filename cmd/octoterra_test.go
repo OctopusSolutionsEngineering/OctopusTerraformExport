9b72a6b437ee420ef980efe76d66c211a3030b66
Matthew Casperson <matthewcasperson@gmail.com>
2025-03-28 15:13:22 +1000

    Fixed tests

--- cmd/octoterra_test.go
+++ cmd/octoterra_test.go
@@ -234,7 +234,7 @@ func exportProjectImportAndTest(
 		[]string{},
 		importSpaceVars,
 		func(url string, space string, apiKey string, dest string) error {
-			projectId, err := entry.ConvertProjectNameToId(url, space, test.ApiKey, projectName, "")
+			projectId, err := entry.ConvertProjectNameToId(url, space, test.ApiKey, "", projectName, "")
 
 			if err != nil {
 				return err
@@ -380,14 +380,14 @@ func exportProjectLookupImportAndTest(
 		prepopulateSpaceVars,
 		importSpaceVars,
 		func(url string, space string, apiKey string, dest string) error {
-			projectId, err := entry.ConvertProjectNameToId(url, space, test.ApiKey, projectName, "")
+			projectId, err := entry.ConvertProjectNameToId(url, space, test.ApiKey, "", projectName, "")
 			if err != nil {
 				return err
 			}
 
 			runbookId := ""
 			if argumnets.RunbookName != "" {
-				runbookId, err = entry.ConvertRunbookNameToId(url, space, test.ApiKey, projectId, argumnets.RunbookName, "")
+				runbookId, err = entry.ConvertRunbookNameToId(url, space, test.ApiKey, "", projectId, argumnets.RunbookName, "")
 
 				if err != nil {
 					return err
