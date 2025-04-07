ff32cdd4ed66993cd77c838e1aff60a71d3b5cde
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-07 15:21:23 +1000

    Added some tests

---
+++ cmd/azure/octoterra_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestHostIsCloudOrLocal(t *testing.T) {
+	tests := []struct {
+		host     string
+		expected bool
+	}{
+		{"example.octopus.app", true},
+		{"example.testoctopus.com", true},
+		{"localhost", true},
+		{"127.0.0.1", true},
+		{"example.com", false},
+		{"192.168.1.1", false},
+	}
+
+	for _, test := range tests {
+		result := hostIsCloudOrLocal(test.host)
+		if result != test.expected {
+			t.Errorf("hostIsCloudOrLocal(%s) = %v; want %v", test.host, result, test.expected)
+		}
+	}
+}
