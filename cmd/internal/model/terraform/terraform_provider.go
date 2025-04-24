267e2fc9d1cbad2aa1ef5cd8cbcd6cd4cbb608e5
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-25 06:07:32 +1000

    Updated provider version

--- cmd/internal/model/terraform/terraform_provider.go
+++ cmd/internal/model/terraform/terraform_provider.go
@@ -45,7 +45,7 @@ func (c TerraformConfig) CreateTerraformConfig(backend string, version string) T
 		RequiredProviders: RequiredProviders{
 			OctopusProvider: ProviderDefinition{
 				Source:  "OctopusDeployLabs/octopusdeploy",
-				Version: strutil.DefaultIfEmpty(version, "0.42.0"),
+				Version: strutil.DefaultIfEmpty(version, "0.43.0"),
 			},
 			ShellProvider: ProviderDefinition{
 				Source:  "scottwinkler/shell",
