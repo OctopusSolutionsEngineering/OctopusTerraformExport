a743b190c7ab81e4bd31ee2cbf30dbd7f448095d
Matthew Casperson <matthewcasperson@gmail.com>
2025-06-20 15:05:24 +1000

    Renamed worker pools to avoid the assumption that they exist. Removed the built-in trigger. Removed the channel data query.

--- test/terraform/89-everystep/space_population/channel_every_step_project_default.tf
+++ test/terraform/89-everystep/space_population/channel_every_step_project_default.tf
@@ -1,6 +1,7 @@
-data "octopusdeploy_channels" "channel_every_step_project_default" {
-  ids          = []
-  partial_name = "Default"
-  skip         = 0
-  take         = 1
-}
+# This is not used, and the default channel is not usable in the TF provider.
+# data "octopusdeploy_channels" "channel_every_step_project_default" {
+#   ids          = []
+#   partial_name = "Default"
+#   skip         = 0
+#   take         = 1
+# }
--- test/terraform/89-everystep/space_population/projecttrigger_every_step_project_built_in_feed_trigger.tf
+++ test/terraform/89-everystep/space_population/projecttrigger_every_step_project_built_in_feed_trigger.tf
@@ -1,10 +1,10 @@
-resource "octopusdeploy_built_in_trigger" "projecttrigger_every_step_project_built_in_feed_trigger" {
-  space_id                 = "${trimspace(var.octopus_space_id)}"
-  channel_id               = octopusdeploy_channel.channel_every_step_project_hotfix.id
-  project_id               = "${octopusdeploy_project.project_every_step_project.id}"
-  release_creation_package = { deployment_action = "Deploy a Package", package_reference = "" }
-  depends_on               = [
-    octopusdeploy_process_steps_order.process_step_order_every_step_project,
-    octopusdeploy_process_step.process_step_every_step_project_deploy_a_package
-  ]
-}
+# resource "octopusdeploy_built_in_trigger" "projecttrigger_every_step_project_built_in_feed_trigger" {
+#   space_id                 = "${trimspace(var.octopus_space_id)}"
+#   channel_id               = octopusdeploy_channel.channel_every_step_project_hotfix.id
+#   project_id               = "${octopusdeploy_project.project_every_step_project.id}"
+#   release_creation_package = { deployment_action = "Deploy a Package", package_reference = "" }
+#   depends_on               = [
+#     octopusdeploy_process_steps_order.process_step_order_every_step_project,
+#     octopusdeploy_process_step.process_step_every_step_project_deploy_a_package
+#   ]
+# }
