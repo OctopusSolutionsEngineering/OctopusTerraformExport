c52f4bd5e570ff91e39d40da12da2e03c3fc8814
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-05 14:17:42 +1000

    Fixed reference

--- cmd/internal/converters/step_template_converter.go
+++ cmd/internal/converters/step_template_converter.go
@@ -373,7 +373,7 @@ func (c StepTemplateConverter) toHcl(template octopus.StepTemplate, communitySte
 
 		var communityActionTemplate string = ""
 		if thisResource.ExternalID != "" {
-			communityActionTemplate = "${" + stepTemplateName + ".CommunityActionTemplateId}"
+			communityActionTemplate = "${shell_script." + stepTemplateName + ".CommunityActionTemplateId}"
 		}
 
 		terraformResource := terraform.TerraformStepTemplate{
