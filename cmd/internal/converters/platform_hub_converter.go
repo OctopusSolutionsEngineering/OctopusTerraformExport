ec49002dfa425256d3f30cbdbb479740f7207735
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-16 18:53:50 +1000

    Refactor PlatformHub HCL generation to separate username/password handling

--- cmd/internal/converters/platform_hub_converter.go
+++ cmd/internal/converters/platform_hub_converter.go
@@ -49,45 +49,53 @@ func (c PlatformHubConverter) allToHcl(all bool, dependencies *data.ResourceDeta
 	thisResource.ResourceType = "PlatformHubVersionControl"
 
 	thisResource.ToHcl = func() (string, error) {
-		terraformResource := terraform.TerraformPlatformHubVersionControlUsernamePasswordSetting{
-			Type:          resource.Credentials.Type,
-			Name:          "PlatformHubVersionControl",
-			Count:         nil,
-			Url:           resource.Url,
-			DefaultBranch: resource.DefaultBranch,
-			BasePath:      resource.BasePath,
-			Username:      resource.Credentials.Username,
-			Password:      "${var.PlatformHubVersionControlPassword}",
-		}
-		file := hclwrite.NewEmptyFile()
-		block := gohcl.EncodeAsBlock(terraformResource, "resource")
-		file.Body().AppendBlock(block)
-
-		secretVariableResource := terraform.TerraformVariable{
-			Name:        "PlatformHubVersionControlPassword",
-			Type:        "string",
-			Nullable:    false,
-			Sensitive:   true,
-			Description: "The secret variable value associated with the platform hub version control settings",
+		if resource.Credentials.Type == "UsernamePassword" {
+			return c.generateUsernamePasswordHcl(resource, dependencies)
 		}
 
-		if c.DummySecretVariableValues {
-			secretVariableResource.Default = c.DummySecretGenerator.GetDummySecret()
-			dependencies.AddDummy(data.DummyVariableReference{
-				VariableName: "PlatformHubVersionControlPassword",
-				ResourceName: "PlatformHubVersionControlPassword",
-				ResourceType: "PlatformHubVersionControl",
-			})
-		}
-
-		block = gohcl.EncodeAsBlock(secretVariableResource, "variable")
-		hcl.WriteUnquotedAttribute(block, "type", "string")
-		file.Body().AppendBlock(block)
-
-		return string(file.Bytes()), nil
+		return "", nil
 	}
 
 	dependencies.AddResource(thisResource)
 
 	return nil
+}
+
+func (c PlatformHubConverter) generateUsernamePasswordHcl(resource *octopus.OctopusPlatformHubVersionControlUsernamePasswordSetting, dependencies *data.ResourceDetailsCollection) (string, error) {
+	terraformResource := terraform.TerraformPlatformHubVersionControlUsernamePasswordSetting{
+		Type:          "octopusdeploy_platform_hub_version_control_username_password_settings",
+		Name:          "PlatformHubVersionControl",
+		Count:         nil,
+		Url:           resource.Url,
+		DefaultBranch: resource.DefaultBranch,
+		BasePath:      resource.BasePath,
+		Username:      resource.Credentials.Username,
+		Password:      "${var.PlatformHubVersionControlPassword}",
+	}
+	file := hclwrite.NewEmptyFile()
+	block := gohcl.EncodeAsBlock(terraformResource, "resource")
+	file.Body().AppendBlock(block)
+
+	secretVariableResource := terraform.TerraformVariable{
+		Name:        "PlatformHubVersionControlPassword",
+		Type:        "string",
+		Nullable:    false,
+		Sensitive:   true,
+		Description: "The secret variable value associated with the platform hub version control settings",
+	}
+
+	if c.DummySecretVariableValues {
+		secretVariableResource.Default = c.DummySecretGenerator.GetDummySecret()
+		dependencies.AddDummy(data.DummyVariableReference{
+			VariableName: "PlatformHubVersionControlPassword",
+			ResourceName: "PlatformHubVersionControlPassword",
+			ResourceType: "PlatformHubVersionControl",
+		})
+	}
+
+	block = gohcl.EncodeAsBlock(secretVariableResource, "variable")
+	hcl.WriteUnquotedAttribute(block, "type", "string")
+	file.Body().AppendBlock(block)
+
+	return string(file.Bytes()), nil
 }
