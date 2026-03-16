9d8c20408c7f27c65dfd4169a694466bcc2e1724
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-16 18:49:47 +1000

    Add PlatformHub version control settings export and secret variable handling

--- cmd/internal/converters/platform_hub_converter.go
+++ cmd/internal/converters/platform_hub_converter.go
@@ -3,13 +3,20 @@ package converters
 import (
 	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/client"
 	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/data"
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/dummy"
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/hcl"
 	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/model/octopus"
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformExport/cmd/internal/model/terraform"
+	"github.com/hashicorp/hcl2/gohcl"
+	"github.com/hashicorp/hcl2/hclwrite"
 	"golang.org/x/sync/errgroup"
 )
 
 type PlatformHubConverter struct {
-	Client   client.OctopusClient
-	ErrGroup *errgroup.Group
+	Client                    client.OctopusClient
+	ErrGroup                  *errgroup.Group
+	DummySecretVariableValues bool
+	DummySecretGenerator      dummy.DummySecretGenerator
 }
 
 func (c PlatformHubConverter) AllToHcl(dependencies *data.ResourceDetailsCollection) {
@@ -25,5 +32,62 @@ func (c PlatformHubConverter) allToHcl(all bool, dependencies *data.ResourceDeta
 	if err := c.Client.GetAllGlobalResources("PlatformHub/VersionControl", &resource, []string{}, []string{}); err != nil {
 		return err
 	}
+
+	// Assume if no URL is set, there is nothing to export
+	if resource.Url == "" {
+		return nil
+	}
+
+	/*
+		The platform hub version control settings are unique in that there is only one setting across the entire space.
+		It has no name or ID.
+	*/
+	thisResource := data.ResourceDetails{}
+	thisResource.FileName = "space_population/platform_hub_version_control.tf"
+	thisResource.Id = "PlatformHubVersionControl"
+	thisResource.Name = "PlatformHubVersionControl"
+	thisResource.ResourceType = "PlatformHubVersionControl"
+
+	thisResource.ToHcl = func() (string, error) {
+		terraformResource := terraform.TerraformPlatformHubVersionControlUsernamePasswordSetting{
+			Type:          resource.Credentials.Type,
+			Name:          "PlatformHubVersionControl",
+			Count:         nil,
+			Url:           resource.Url,
+			DefaultBranch: resource.DefaultBranch,
+			BasePath:      resource.BasePath,
+			Username:      resource.Credentials.Username,
+			Password:      "${var.PlatformHubVersionControlPassword}",
+		}
+		file := hclwrite.NewEmptyFile()
+		block := gohcl.EncodeAsBlock(terraformResource, "resource")
+		file.Body().AppendBlock(block)
+
+		secretVariableResource := terraform.TerraformVariable{
+			Name:        "PlatformHubVersionControlPassword",
+			Type:        "string",
+			Nullable:    false,
+			Sensitive:   true,
+			Description: "The secret variable value associated with the platform hub version control settings",
+		}
+
+		if c.DummySecretVariableValues {
+			secretVariableResource.Default = c.DummySecretGenerator.GetDummySecret()
+			dependencies.AddDummy(data.DummyVariableReference{
+				VariableName: "PlatformHubVersionControlPassword",
+				ResourceName: "PlatformHubVersionControlPassword",
+				ResourceType: "PlatformHubVersionControl",
+			})
+		}
+
+		block = gohcl.EncodeAsBlock(secretVariableResource, "variable")
+		hcl.WriteUnquotedAttribute(block, "type", "string")
+		file.Body().AppendBlock(block)
+
+		return string(file.Bytes()), nil
+	}
+
+	dependencies.AddResource(thisResource)
+
 	return nil
 }
